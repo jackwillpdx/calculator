785781cf5c6156c89d43adbf40b3fe2a3a466da4
Jack Williams <jackwillpdx@gmail.com>
2023-10-16 15:13:22 -0700

    updated functions
    Co-Authored-By Ravin Fisher <ravesterrr@gmail.com>

---
+++ calculator/JS/scripts.js
@@ -0,0 +1,28 @@
+// business logic
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+// user interface logic 
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert(" Sum of added numbers = " + add(number1, number2));
+
+window.alert(" Sum of subtracted numbers = " + (subtract(number1, number2)));
+
+window.alert(" Sum of multiplied numbers = " + multiply(number1, number2));
+
+window.alert(" Sum of divided numbers = " + divide(number1, number2));
